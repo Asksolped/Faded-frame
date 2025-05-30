6eda312b27d5ee47062c0e17c6fd463c8b95c169
Ask Solheim <solheimen21@gmail.com>
2025-05-30 08:41:38 +0200

    Props update

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,4 +1,12 @@
-function Card(item) {
+import PropTypes from "prop-types";
+
+function Card(
+  item,
+
+  productName = "Placehodler name",
+  descriptionShort = "Placeholder for short description",
+  descriptionextended = "Placeholder for long description"
+) {
   return (
     <div className="shop-card" id={item.id}>
       <img
@@ -8,12 +16,19 @@ function Card(item) {
       />
       <div className="card-text">
         <div className="carousel-snapper"></div>
-        <h2>Product Name</h2>
-        <p className="always-show-text">Short description</p>
-        <p className="hover-show-text">Extended description</p>
+        <h2>{productName}</h2>
+        <p className="always-show-text">{descriptionShort}</p>
+        <p className="hover-show-text">{descriptionextended}</p>
       </div>
     </div>
   );
 }
+
+Card.propTypes = {
+  item: PropTypes.object.isRequired,
+  productName: PropTypes.string,
+  descriptionShort: PropTypes.string,
+  descriptionextended: PropTypes.string,
+};
 
 export default Card;
